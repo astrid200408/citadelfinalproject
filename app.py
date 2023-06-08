da5e43bd8df65d7a183ff578e50754e4acf10172
astrid200408 <astridsanabria4@gmail.com>
2023-06-08 13:53:32 -0400

    updating images

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask, render_template
 import requests
 from model import get_questions
+import json
+
 
 
 app = Flask(__name__)
+question_num = 4
+level = 1
+curr_answer = ""
 
 @app.route("/", methods = ["GET", "POST"])
 @app.route("/index")
@@ -19,10 +24,49 @@ def levels():
 def financialvocab():
     return render_template("financialvocab.html")
 
+@app.route("/marketmaking", methods = ["GET", "POST"])
+def marketmaking():
+    return render_template("marketmaking.html")
+
 @app.route("/game", methods = ["GET", "POST"])
 def game():
-    props= get_questions()
+    global question_num
+    global level
+    questions = get_questions()
+    props = questions[level][question_num]
+    curr_answer = props["answer"]
+    print(curr_answer)
+    question_num -= 1
     return render_template("game.html", props=props)
+
+@app.route("/check_a", methods = ["GET", "POST"])
+def check_a():
+    if curr_answer == "a":
+        return render_template("correct.html")
+    else:
+        return render_template("incorrect.html")
+
+@app.route("/check_b", methods = ["GET", "POST"])
+def check_b():
+    if curr_answer == "b":
+        return render_template("correct.html")
+    else:
+        return render_template("incorrect.html")
+
+@app.route("/check_c", methods = ["GET", "POST"])
+def check_c():
+    if curr_answer == "c":
+        return render_template("correct.html")
+    else:
+        return render_template("incorrect.html")
+
+@app.route("/check_d", methods = ["GET", "POST"])
+def check_d():
+    if curr_answer == "d":
+        return render_template("correct.html")
+    else:
+        return render_template("incorrect.html")
+
 
 
 
--- model.py
+++ model.py
@@ -1,12 +1,44 @@
 import os
+import json
 
 def get_questions():
 
-    questions = []
-    f = open('questions.txt','r')
+    questions = {}
+    f = open('questions.txt', 'r')
+
+    quest_count = 5;
+    level_count = 1;
 
     for x in f:
-        questions.append(x)
+        
+        my_dict = json.loads(x)
+        print(my_dict["question"])
 
+        if quest_count ==5 :
+            # add first dict to level
+            new_list = []
+            new_list.append(my_dict)
+            quest_count-=1
+
+        elif quest_count == 1:
+
+            # adds last dict to level and 
+            new_list.append(my_dict)
+            questions[level_count] = new_list
+
+            # go to next level 
+            quest_count == 5
+            level_count+=1
+
+        else:
+
+            new_list.append(my_dict)
+            quest_count -= 1
+    
     f.close()
+
+    return questions
+
+print(get_questions())
+    
 
